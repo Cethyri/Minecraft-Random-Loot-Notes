4029c6a638c258ac11ee5cff4aacb4642f34b1fc
Christopher Robinson <seabug7200@hotmail.com>
2021-05-22 22:57:52 -0700

    format

--- mcr/flags.py
+++ mcr/flags.py
@@ -5,38 +5,40 @@ from mcr.helpers.regex import remove_initial_dashes
 
 flagFile = 'mcr/data/flags.json'
 
+
 def flagHelp(json_flags: dict[str, Any]):
-	for flag, flagInfo in json_flags.items():
-		print(f'{flag}:')
-		print(f'    {flagInfo["explanation"]}')
-		if 'warning' in flagInfo:
-			print(f'    [Warning] {flagInfo["warning"]}')
-		print()
+    for flag, flagInfo in json_flags.items():
+        print(f'{flag}:')
+        print(f'    {flagInfo["explanation"]}')
+        if 'warning' in flagInfo:
+            print(f'    [Warning] {flagInfo["warning"]}')
+        print()
+
 
 def handleFlags(args: List[str]):
-	with open(flagFile) as json_file:
-		json_flags = json.load(json_file)
+    with open(flagFile) as json_file:
+        json_flags = json.load(json_file)
 
-	if len(args) > 1 and 'help' in args[1]:
-		flagHelp(json_flags)
-		return None
+    if len(args) > 1 and 'help' in args[1]:
+        flagHelp(json_flags)
+        return None
 
-	print('Checking flags and setting up...')
+    print('Checking flags and setting up...')
 
-	flags: dict[str, Any] = {}
-	for flag in json_flags:
-		flags[flag] = False
+    flags: dict[str, Any] = {}
+    for flag in json_flags:
+        flags[flag] = False
 
-	for arg in args:
-		arg = remove_initial_dashes(arg)
-		if arg in flags:
-			flags[arg] = True
-		elif arg.isdigit():
-			flags['seed'] = arg
-		elif '=' in arg:
-			setArg = arg.split('=')
-			flags[setArg[0]] = setArg[1]
-		else:
-			print(f'{arg} is not a recognized flag, ignoring...')
+    for arg in args:
+        arg = remove_initial_dashes(arg)
+        if arg in flags:
+            flags[arg] = True
+        elif arg.isdigit():
+            flags['seed'] = arg
+        elif '=' in arg:
+            setArg = arg.split('=')
+            flags[setArg[0]] = setArg[1]
+        else:
+            print(f'{arg} is not a recognized flag, ignoring...')
 
-	return flags
+    return flags
